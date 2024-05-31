ecf4facbf42d197f9c5e95a565c25685770d6e00
Elijah Elliott <elliott.t.elijah@gmail.com>
2024-05-30 21:13:01 -0600

    Added comments

--- src/zapmoto_interview_typescript.ts
+++ src/zapmoto_interview_typescript.ts
@@ -1,26 +1,25 @@
-import * as readline from 'readline';
 import * as readlineSync from 'readline-sync';
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+
 const origianlBoard: string[][] = [
     ['-', '-', '-'],
     ['-', '-', '-'],
     ['-', '-', '-']
   ];
 
+// Displays the Board
 function displayBoard(board: string[][]) {
     console.log('\n', board[0][0], ' ', board[1][0], ' ', board[2][0], '\n', board[0][1], ' ', board[1][1], ' ', board[2][1],'\n', board[0][2], ' ', board[1][2], ' ', board[2][2])
 }
 
+// Keeps track of whose turn it is
 function countTurn (count: number) {
     if (count == 1) 
         return count = 0
     return count = 1
 }
 
+// Gives the player their value. X or O
 function playerTurn(player: number){
     if (player == 1) {
         return 'O'
@@ -28,6 +27,7 @@ function playerTurn(player: number){
     return 'X'
 }
 
+// Changes the value inside the Board array to the user's input
 function playerChoice (userInput: number, game: string[][], playerTurn: string) {
     if (userInput == 1) {
         if (game[0][0] == '-'){
@@ -103,6 +103,7 @@ function playerChoice (userInput: number, game: string[][], playerTurn: string) 
     }
 }
 
+// Checks to see if the player is a winner
 function isPlayerWinner(game: string[][] ) {
     let isWinner = false
     // Checking Vertically
@@ -128,16 +129,19 @@ function isPlayerWinner(game: string[][] ) {
     return isWinner
   }
 
-  function isPlay(): boolean {
+// Checks to see if the players want to conutie playing
+function isPlay(): boolean {
     const answer = readlineSync.question("Do you want to play Tic Tac Toe?\nYes or No? ");
     return answer.toLowerCase() === 'yes';
-  }
+}
 
+// Ask the user where they want to put their X or O
 function askMove(): number {
     const answer = readlineSync.question('Enter your move (1-9): ');
     return parseInt(answer, 10);
 }
 
+// Resets the board
 function clearBoard() {
     return [
         ['-', '-', '-'],
@@ -146,6 +150,7 @@ function clearBoard() {
       ]
 }
 
+// Allows the users to play Tic Tac Toe
 function playGame() {
     let move = 0
     let play = isPlay()
